14a664fadc5bb822fe9017f4ea44f643f9d3df24
Simon Lagos <simon.lagos.sallhed@gmail.com>
2016-04-23 01:20:16 +0200

    Load commands from backend

--- app/public/js/commands.js
+++ app/public/js/commands.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
 import Command from './command';
+import fetch from 'isomorphic-fetch';
 
 export default class Commands extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      commands: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('api/command')
+    .then((response)=>{
+      return response.json()
+    })
+    .then((json)=>{
+      this.setState({commands: json})
+      console.log(this.state);
+    })
   }
 
   render(){
