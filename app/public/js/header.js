610ca686015684e7d368e0751ff9cce5b363d15b
erdalbej <erdbej@gmail.com>
2016-04-22 22:14:14 +0200

    stuff

---
+++ app/public/js/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		var style = {
+
+		};
+		return (
+			<div>
+				<img src="#">
+				
+			</div>
+		)
+	}
+}
