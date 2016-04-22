f4e3ed550b188a6472453cf842fb10e2fa064c75
Simon Lagos <simon.lagos.sallhed@gmail.com>
2016-04-22 22:45:15 +0200

    First version of devices structure

--- app/public/js/device.js
+++ app/public/js/device.js
@@ -3,28 +3,17 @@ import React, {Component} from 'react';
 export default class Device extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      devices: [
-        "Coffee Maker",
-        "Lights",
-        "Spotify"
-      ]
-    }
   }
   render(){
-    var style = {
-      display: "flex",
-      width: 400,
-      justifyContent: "space-between",
-      background: "yellow"
-    };
+    // var style = {
+    //   display: "flex",
+    //   width: 400,
+    //   justifyContent: "space-between",
+    //   background: "yellow"
+    // };
     return(
-      <div style={style}>
-        {
-          this.state.devices.map(function(device){
-            return <p>{device}</p>;
-          })
-        }
+      <div>
+        <p>{this.props.name}</p>
       </div>
     )
   }
--- app/public/js/devices.js
+++ app/public/js/devices.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
+import devices from '../devices.json';
+import Device from './device';
 
-export default class Devices from Component {
+export default class Devices extends Component {
   constructor(props){
     super(props);
   }
 
   render(){
-    render(
+    return(
       <div>
-        Devices
+      { devices.devices.map((device)=>{
+        return <Device key={device.name} name={device.name} />
+      })}
       </div>
     )
   }
