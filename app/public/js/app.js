1d25dfa4a467cc7655bae415166a755255d5e24c
Simon Lagos <simon.lagos.sallhed@gmail.com>
2016-04-22 23:32:07 +0200

    Merge

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -4,12 +4,17 @@ import Device from './device';
 import Header from './header';
 import Devices from './devices';
 import Commands from './commands';
+import RostWrapper from './rost_wrapper'
 
 export default class App extends Component{
   constructor(){
     super();
   }
   render(){
+    var style={
+      background: "lightblue",
+      height: 1000
+    }
     return(
       <div>
         <Header />
