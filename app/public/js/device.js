4c2cc694d47bd32130a60de2d030227211a7d8af
Simon Lagos <simon.lagos.sallhed@gmail.com>
2016-04-23 01:40:05 +0200

    Display icons in devices aswell

--- app/public/js/device.js
+++ app/public/js/device.js
@@ -17,14 +17,34 @@ export default class Device extends Component {
     var header = {
       padding: "10px"
     }
+
+    var optionsStyle = {
+      display: "flex",
+      justifyContent: "flex-start"
+    };
+
+    var option = {
+      background: "#f0c29c",
+      color: "#fff",
+      textAlign: "center",
+      lineHeight: "10px",
+      padding: "5px",
+      borderRadius: "4px",
+      marginLeft: "10px"
+    }
+
     return(
       <div style={style}>
         <p style={header}>{this.props.name}</p>
+        <div style={optionsStyle}>
         {
           this.props.signals.map((signal)=>{
-            return <p key={signal.id}>{signal.id}</p>
+            return <div key={signal.id} style={option}>
+              <img src="../images/light-on.png" width="24" height="22"/>
+            </div>
           })
         }
+        </div>
 
       </div>
     )
