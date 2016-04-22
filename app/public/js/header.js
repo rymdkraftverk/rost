a0228a9bf4d4709165d02efa9b2815246690eef9
erdalbej <erdbej@gmail.com>
2016-04-23 01:44:05 +0200

    working on the header

--- app/public/js/header.js
+++ app/public/js/header.js
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
 	render(){
 		var style = {
 			background: "#e9a065",
-			padding: "25px 0 20px 20px",
+			padding: "20px 0 15px 20px",
 			display: "flexbox",
 			justifyContent: "space-between"
 		};
