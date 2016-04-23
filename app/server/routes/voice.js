8d6e836d170c7b8d4a633d37d85af591eca81949
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-04-23 11:56:57 +0200

    Aggregating strict and fuzzy signals

--- app/server/routes/voice.js
+++ app/server/routes/voice.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const commandDb = require('../../database/command')
+const request = require('request-promise')
 
 router.post('/', (req, res) => {
 	const voice = req.body.voice
-	matchingStrict(voice)
-	.then(signals => {
+	const fuzzy = matchingFuzzy(voice)
+	const strict = matchingStrict(voice)
+
+	Promise.all([fuzzy, strict])
+	.then(results => {
+		const signals = results.reduce((prev, current) => {
+			const signals = commandSignals(current)
+			return signals.concat(prev)
+		}, [])
 		res.json(signals)
 	})
 	.catch(err => {
@@ -13,24 +21,34 @@ router.post('/', (req, res) => {
 	})
 })
 
+const matchingFuzzy = voice => {
+	const options = {
+		method: 'GET',
+		uri: 'http://localhost:6034/api/command', //TODO
+		body: { voice },
+		json: true
+	}
+	return request(options)
+}
+
 const matchingStrict = voice => {
 	return commandDb.list('strict')
 	.then(result => {
-		const commands = matchingCommands(voice, result)
-		const signals = commands
-		.map(item => item.signals)
-		.reduce((prev, current) => {
-			return prev.concat(current)
-		}, [])
-
-		return signals
+		return result.filter(el => {
+			return voice.includes(el.command)
+		})
 	})
 }
 
-const matchingCommands = (string, list) => {
-	return list.filter(el => {
-		return string.includes(el.command)
-	})
+const commandSignals = commands => {
+	const signals = commands
+	.map(item => item.signals)
+	.filter(item => item)
+	.reduce((prev, current) => {
+		return prev.concat(current)
+	}, [])
+
+	return signals
 }
 
 module.exports = router
