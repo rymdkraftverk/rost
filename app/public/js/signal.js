6206d2be8b9c57a8133c3917a9beaf99289d8740
Simon Lagos <simon.lagos.sallhed@gmail.com>
2016-04-23 10:40:36 +0200

    Make signal icons draggable

--- app/public/js/signal.js
+++ app/public/js/signal.js
@@ -4,6 +4,12 @@ export default class Signal extends Component{
   constructor(props){
     super(props);
   }
+
+  drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+
   render(){
 
     var option = {
@@ -15,10 +21,10 @@ export default class Signal extends Component{
       borderRadius: "4px",
       marginLeft: "10px"
     }
-    
+
     return(
-      <div style={option}>
-        <img src="../images/light-on.png" width="24" height="22"/>
+      <div style={option} draggable="true" ondragstart={this.drag}>
+        <img src="../images/light-on.png" draggable="false" width="24" height="22"/>
       </div>
     )
   }
