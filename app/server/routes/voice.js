fdb0293ee9c361d4dd53e127b02df61a93fe10f7
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-04-23 10:18:41 +0200

    Move logic to route

--- app/server/routes/voice.js
+++ app/server/routes/voice.js
@@ -1,25 +1,36 @@
 const express = require('express')
 const router = express.Router()
 const commandDb = require('../../database/command')
-const util = require('../../util')
 
 router.post('/', (req, res) => {
 	const voice = req.body.voice
-
-	commandDb.list('strict')
-	.then(result => {
-		const commands = util.matchingCommands(voice, result)
-		const signals = commands
-		.map(item => item.signals)
-		.reduce((prev, current) => {
-			return prev.concat(current)
-		}, [])
-
+	matchingSignals(voice)
+	.then(signals => {
 		res.json(signals)
 	})
 	.catch(err => {
 		res.status(400).json(err)
 	})
 })
+
+const matchingSignals = voice => {
+	return commandDb.list('strict')
+	.then(result => {
+		const commands = matchingCommands(voice, result)
+		const signals = commands
+		.map(item => item.signals)
+		.reduce((prev, current) => {
+			return prev.concat(current)
+		}, [])
+
+		return signals
+	})
+}
+
+const matchingCommands = (string, list) => {
+	return list.filter(el => {
+		return string.includes(el.command)
+	})
+}
 
 module.exports = router
--- app/util/index.js
+++
@@ -1,9 +0,0 @@
-const matchingCommands = (string, list) => {
-	return list.filter(el => {
-		return string.includes(el.command)
-	})
-}
-
-module.exports = {
-	matchingCommands
-}
